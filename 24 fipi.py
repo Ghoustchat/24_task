533b98f98ae2b4f8eb763489e50f8fc627ae8206
Ghoustchat <49339918+Ghoustchat@users.noreply.github.com>
2019-04-06 17:32:50 +0500

    fixed the bags

--- 24 fipi.py
+++ 24 fipi.py
@@ -1,13 +1,12 @@
 n = int(input())
-minDigit = n % 10
+minDigit = n
 while n > 0:
     digit =  n % 10
     if digit % 2 == 0:
         if digit < minDigit:
             minDigit = digit
     n = n // 10
-if minDigit == 0:
+if minDigit % 2 !=0:
     print("NO")
 else:
     print(minDigit)
-    
